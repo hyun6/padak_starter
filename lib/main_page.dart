de6c83b1186c80f25f9d06a23c2596254ae306b0
hyun6 <psymhs@gmail.com>
2020-09-30 21:10:25 +0900

    127p 기본 레이아웃까지 작성
     - windows desktop 환경으로 진행 중

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,10 +1,15 @@
-import "package:flutter/material.dart";
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+enum MovieSortOrder { ReservationRate, Curation, Latest }
 
 // 1-2. 탭 화면 (각 화면 import)
 
 // 1-2. 탭 화면 (Stateless -> Stateful)
 class MainPage extends StatelessWidget {
   // 1-2. 탭 화면 (_selectedTabIndex 변수 옮김)
+  int _selectedTabIndex = 0;
   // 1-2. 탭 화면 (탭 인덱스 설정)
 
   // 1-2. 탭 화면 (createState 함수 추가)
@@ -15,15 +20,41 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         // 1-1. 상단화면 (제목 수정)
-        title: Text('Flutter'),
+        title: Text('Movie'),
         // 1-1. 상단화면 (좌측 버튼 추가)
-
+        //leading: Icon(Icons.menu),
         // 1-1. 상단화면 (우측 팝업 버튼 및 이벤트 추가)
-
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.sort),
+            onSelected: (value) {
+              log(value.toString());
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                    value: MovieSortOrder.ReservationRate, child: Text('예매율순')),
+                PopupMenuItem(
+                    value: MovieSortOrder.Curation, child: Text('큐레이션')),
+                PopupMenuItem(value: MovieSortOrder.Latest, child: Text('최신순')),
+              ];
+            },
+          )
+        ],
       ),
       // 1-2. 탭 화면 (List, Grid Widget 연동)
-      body: Center(child: Text("Hello Flutter")),
+      body: Center(child: Text('Hello Flutter')),
       // 1-2. 탭 화면 (bottomNavigationBar 추가)
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.view_list), label: 'List'),
+          BottomNavigationBarItem(icon: Icon(Icons.grid_on), label: 'Grid')
+        ],
+        currentIndex: _selectedTabIndex,
+        onTap: (index) {
+          _selectedTabIndex = index;
+        },
+      ),
     );
   }
 }
